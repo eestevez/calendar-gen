73ded3866401488cb7698cb99c9468fba4692feb
eesteve <eds2@openmailbox.org>
2018-09-30 23:57:39 -0500

    Adding new components, module and provider to project main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MonthlyCalendarComponent } from './calendar/monthly-calendar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HolidayService } from './service/holiday.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, CalendarComponent, MonthlyCalendarComponent ],
+  providers: [HolidayService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
